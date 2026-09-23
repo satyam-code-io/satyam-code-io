b190cbb376605f1a10db72e24be09975f445ef19
Satyam Choudhary <chaudharysatyam12@gmail.com>
2026-09-23 13:23:56 +0530

    Moved files into folders

---
+++ Basics/checkcharacter.java
@@ -0,0 +1,118 @@
+
+//import java.util.Scanner;
+
+// public class checkcharacter{ 
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         String name=sc.next();
+//         String str="";
+//         str=str+name.replace('e','i');
+//         System.out.println(str);
+//     }
+// }
+// import java.util.*;
+
+
+// public class work {
+//    public static void main(String args[]) {
+//      Scanner sc = new Scanner (System.in);
+//      String str = sc.next();
+//      String result = "";
+
+
+//      for(int i=0; i<str.length(); i++) {
+//        if(str.charAt(i) == '@') {
+//         break;
+
+         
+//        } else {
+//          result += str.charAt(i);
+//        }
+//      }
+
+
+//      System.out.println(result);
+//    }
+// }
+
+
+            
+
+        
+
+            
+        
+        
+    
+
+                
+            
+        
+            
+              
+
+              
+        
+
+        
+           
+    
+
+//import java.util.Scanner;
+
+// public class checkcharacter{ 
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         String name=sc.next();
+//         String str="";
+//         str=str+name.replace('e','i');
+//         System.out.println(str);
+//     }
+// }
+// import java.util.*;
+
+
+// public class work {
+//    public static void main(String args[]) {
+//      Scanner sc = new Scanner (System.in);
+//      String str = sc.next();
+//      String result = "";
+
+
+//      for(int i=0; i<str.length(); i++) {
+//        if(str.charAt(i) == '@') {
+//         break;
+
+         
+//        } else {
+//          result += str.charAt(i);
+//        }
+//      }
+
+
+//      System.out.println(result);
+//    }
+// }
+
+
+            
+
+        
+
+            
+        
+        
+    
+
+                
+            
+        
+            
+              
+
+              
+        
+
+        
+           
+    
